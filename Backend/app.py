e9a5598a2e59ad5368a6bd24199d8a91fddec72e
TylersHub <18tyler.rosa1@gmail.com>
2024-08-07 23:22:27 -0400

    Creating Product Class for database and the database itself

--- Backend/app.py
+++ Backend/app.py
@@ -2,6 +2,21 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///product_data.db'
+db = SQLAlchemy(app)
+
+class Product(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(1000))
+    image = db.Column(db.String(1000))
+    url = db.Column(db.String(1000))
+    price = db.Column(db.String(1000))
+
+    def __init__(self, name, image, url, price):
+        self.name = name
+        self.image = image
+        self.url = url
+        self.price = price
 
 @app.route('/')
 def message():
