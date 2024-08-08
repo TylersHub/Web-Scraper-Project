374b53bacc8d912db1439eaf640dbc033d0595d6
TylersHub <18tyler.rosa1@gmail.com>
2024-08-08 00:36:51 -0400

    Creating ProductCard component and implementing into App

--- Frontend/react-env/src/App.tsx
+++ Frontend/react-env/src/App.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Button from "./components/Button";
 import InputBox from "./components/InputBox";
+import ProductCard from "./components/ProductCard";
 
 const App = () => {
   return (
     <div>
       <p className="text-center fs-1 fw-semibold">Web Scraper</p>
       <InputBox inputLabel="Search Bar" inputPlaceholder="Search" />
+      <ProductCard id={1} name="Product1" image="" url="" />
       <Button />
     </div>
   );
---
+++ Frontend/react-env/src/components/ProductCard.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+
+interface Props {
+  id: number;
+  name?: string;
+  image?: string;
+  url?: string;
+  price?: number;
+}
+
+const ProductCard = ({
+  id,
+  name = "N/A",
+  image = "No image",
+  url = "N/A",
+  price = 0,
+}: Props) => {
+  return (
+    <div className="card" style={{ width: "18rem" }}>
+      <img src={image} className="card-img-top" alt="..." />
+      <div className="card-body">
+        <h5 className="card-title">{price}</h5>
+        <p className="card-text">
+          {name} {id}
+        </p>
+        <a href={url} className="btn btn-primary">
+          View Product
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default ProductCard;
